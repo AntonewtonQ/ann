ff645e33991f6fcdbfb77ced94bc9cd0eebd5562
Antonewton Quima <104485879+AntonewtonQ@users.noreply.github.com>
2024-03-29 09:08:42 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById('btnSwitch').addEventListener('click',()=>{
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme','light')
+        document.getElementById('switch-mode').setAttribute('class','bi bi-brightness-high-fill');
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme','dark')
+        document.getElementById('switch-mode').setAttribute('class','bi bi-moon-stars-fill');
+    }
+})
